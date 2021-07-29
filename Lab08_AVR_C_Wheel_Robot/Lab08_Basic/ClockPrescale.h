66742c77453cc3995211f54b9519ab4395d2e97d
KoKoLates <87475042+KoKoLates@users.noreply.github.com>
2021-07-30 07:56:17 +0800

    Lab08

--- Lab08_AVR_C_Wheel_Robot/Lab08_Basic/ClockPrescale.h
+++ Lab08_AVR_C_Wheel_Robot/Lab08_Basic/ClockPrescale.h
@@ -1,10 +1,3 @@
-/*
- * Version 
- * Author: WildfootW
- * GitHub: github.com/WildfootW
- * Copyleft (C) 2020 WildfootW all rights reversed
- *
- */
 
 #ifndef CLOCKPRESCALE_H
 #define CLOCKPRESCALE_H
