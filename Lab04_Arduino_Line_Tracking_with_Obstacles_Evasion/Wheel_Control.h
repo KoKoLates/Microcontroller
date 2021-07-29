733582efb6f18c563ba895652e629bf21a67a300
KoKoLates <87475042+KoKoLates@users.noreply.github.com>
2021-07-30 07:52:54 +0800

    Lab04

--- Lab04_Arduino_Line_Tracking_with_Obstacles_Evasion/Wheel_Control.h
+++ Lab04_Arduino_Line_Tracking_with_Obstacles_Evasion/Wheel_Control.h
@@ -1,10 +1,3 @@
-/*
- * Version
- * Author: WildfootW
- * GitHub: github.com/WildfootW
- * Copyleft (C) 2019-2020 WildfootW all rights reversed
- *
- */
 
 #ifndef WHEEL_CONTROL_H
 #define WHEEL_CONTROL_H
