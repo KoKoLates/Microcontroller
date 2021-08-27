89408286f26f99fcdb330480da22886ce9b79f72
KoKoLates <87475042+KoKoLates@users.noreply.github.com>
2021-08-27 18:57:58 +0800

    Delete ClockPrescale.h

--- Lab08_AVR_C_Wheel_Robot/Lab08_Basic/ClockPrescale.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef CLOCKPRESCALE_H
-#define CLOCKPRESCALE_H
-typedef struct ClockPrescale { } ClockPrescale;
-void ClockPrescale__change_enable(ClockPrescale* self);
-void ClockPrescale__select(ClockPrescale* self, uint16_t prescale);
-#endif // CLOCKPRESCALE_H
